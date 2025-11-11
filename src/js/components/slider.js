6257606611a3751054af92163f7dd0a6151a1c88
Narvik1221 <Narvik1221@mail.ru>
2025-11-12 00:42:58 +0600

    main

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,62 +1,31 @@
-// Инициализация слайдера
 function initSlider() {
     const sliderElement = document.querySelector('.main-slider');
     
-    // Проверяем, есть ли слайдер на странице
     if (!sliderElement) return;
     
     const mainSlider = new Swiper('.main-slider', {
-        // Динамическое количество слайдов
+        // Автоматическое количество слайдов
         slidesPerView: 'auto',
-        spaceBetween: 30,
         centeredSlides: true,
         loop: true,
+      
         
         // Навигация стрелками
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
-        
-        // Адаптивность
-        breakpoints: {
-            // 320px и выше
-            320: {
-                spaceBetween: 15,
-                centeredSlides: true,
-            },
-            // 768px и выше
-            768: {
-                spaceBetween: 20,
-                centeredSlides: true,
-            },
-            // 1340px и выше
-            1340: {
-                spaceBetween: 25,
-                centeredSlides: true,
-            },
-            // 1920px и выше
-            1920: {
-                spaceBetween: 30,
-                centeredSlides: true,
-            }
-        },
-        
+
         // Эффекты
         effect: 'slide',
         speed: 600,
         
-        // Автопрокрутка (опционально)
-        autoplay: {
-            delay: 5000,
-            disableOnInteraction: false,
-        },
+        // Автопрокрутка
+        // autoplay: {
+        //     delay: 5000,
+        //     disableOnInteraction: false,
+        // },
     });
 
     console.log('Слайдер инициализирован');
-}
-
-// Экспортируем функцию для использования в других местах
-if (typeof module !== 'undefined' && module.exports) {
-    module.exports = { initSlider };
 }
